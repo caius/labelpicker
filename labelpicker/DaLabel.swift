4d3cddf54242f1efc6c4d112eb986d3451ed8e30
Caius Durling <dev@caius.name>
2018-01-28 23:07:33 +0000

    Add label & make it DaLabel

---
+++ labelpicker/DaLabel.swift
@@ -0,0 +1,21 @@
+//
+//  DaLabel.swift
+//  labelpicker
+//
+//  Created by Caius Durling on 28/01/2018.
+//  Copyright © 2018 Caius Durling. All rights reserved.
+//
+
+import UIKit
+
+class DaLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
